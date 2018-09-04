88a17db3c15050cbb1e6c9b3e0d169a1f147b131
Aleksandr <38788523+sansanich83@users.noreply.github.com>
2018-09-04 17:22:43 +0300

    Оформление контента

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 var hidebutton = document.querySelector(".form-title");
 var formtoggle = document.querySelector(".search-form");
 
+
 hidebutton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  formtoggle.classList.toggle("hide-form");
+  formtoggle.classList.toggle("search-form-show");
 });
+
+function changePeoplesNumber(e) {
+  console.log(e.id);
+  var inputAdults = document.getElementById("adults-number");
+  if (e.id === "button-minus-adults" && inputAdults.value > 0) {
+    inputAdults.value--;
+  }
+  if (e.id === "button-plus-adults") {
+    inputAdults.value++;
+  }
+  var inputChilds = document.getElementById("childs-number");
+  if (e.id === "button-minus-childs" && inputChilds.value > 0) {
+    inputChilds.value--;
+  }
+  if (e.id === "button-plus-childs") {
+    inputChilds.value++;
+  }
+}
